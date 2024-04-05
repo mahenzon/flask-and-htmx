fd3b3bda3863a813d86ea9707c548833ebf5ad1c
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-04-05 19:38:24 +0300

    Handle boosts

--- utils/helpers.py
+++ utils/helpers.py
@@ -3,5 +3,6 @@ from flask import request
 
 def is_background_request():
     is_hx_request = request.headers.get("Hx-Request")
+    is_hx_boosted = request.headers.get("Hx-Boosted")
 
-    return bool(is_hx_request)
+    return bool(is_hx_request and not is_hx_boosted)
