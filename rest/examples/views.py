c0b0aeb7d2e1218a67a755b9dd46d8e6f2d306f3
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-04-05 18:46:41 +0300

    Add example hover

--- rest/examples/views.py
+++ rest/examples/views.py
@@ -26,3 +26,14 @@ def handle_ping():
     if request.method == "POST":
         return "Pong!"
     return render_template("examples/ping/show-ping.html")
+
+
+@app.route(
+    "/hover/",
+    methods=["GET", "POST"],
+    endpoint="hover",
+)
+def handle_hover():
+    if request.method == "POST":
+        return "I see you! (only once)"
+    return render_template("examples/hover/show-hover.html")
